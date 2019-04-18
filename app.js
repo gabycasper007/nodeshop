bb4ea5fbea84ff10129026bd162efc6192b1f091
gabrielvasile <gabriell.vasile@gmail.com>
2019-04-18 15:40:16 +0800

    Routing

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/add-product", (req, res) => {
-  res.send(
-    '<form action="product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>'
-  );
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body.title);
-  res.redirect("/");
-});
-
-app.get("/", (req, res) => {
-  res.send("This is the homepage");
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use("/", (req, res) => {
+  res.status(404).send("<h1>These are not the druids you're looking for</h1>");
 });
 
 app.listen(3000);
